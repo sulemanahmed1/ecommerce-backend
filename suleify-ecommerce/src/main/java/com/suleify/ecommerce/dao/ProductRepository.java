c576941052e838f1ac96f1dfb179f49edeee78ce
suleman ahmed <ahmedsuleman@outlook.dk>
2020-06-17 12:54:36 +0200

    changing to war and java 1.8 for server

--- suleify-ecommerce/src/main/java/com/suleify/ecommerce/dao/ProductRepository.java
+++ suleify-ecommerce/src/main/java/com/suleify/ecommerce/dao/ProductRepository.java
@@ -13,4 +13,6 @@ import com.suleify.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+	
+	Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
